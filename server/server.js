d4ecdb6e82fb3779304fc2b642339db38bbb7271
JaimeQuezadaJr <jaimequezadajr@gmail.com>
2023-04-20 14:06:42 -0700

    added server imports

--- server/server.js
+++ server/server.js
@@ -1,1 +1,7 @@
 // imports
+require('./config/mongoose.config');
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const port = process.env.PORT || 4000;
