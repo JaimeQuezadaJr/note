2d0399a9175e8d4d7f61dc166070be771ef3f9c9
JaimeQuezadaJr <jaimequezadajr@gmail.com>
2023-04-21 13:48:04 -0700

    added express cookie and jwt module to app in server

--- server/server.js
+++ server/server.js
@@ -5,3 +5,16 @@ const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const port = process.env.PORT || 4000;
+
+// create express app object
+const app = express();
+
+// express
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+// cookie & jwt
+app.use(cookieParser());
+app.use(cors({ origin: 'http://localhost:3000', credentials: true}));
+
+// routes for login & registration
