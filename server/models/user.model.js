ef388c25a442217af8a5ff28408a216f4955b442
JaimeQuezadaJr <jaimequezadajr@gmail.com>
2023-05-01 18:37:27 -0700

    added firstname lastname email password to userschema

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,2 +1,29 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+
+const UserSchema = mongoose.Schema(
+    {
+        firstName: {
+            type: String,
+            required: [true, 'First Name is required'],
+        },
+        lastName: {
+            type: String,
+            required: [true, 'Last Name is required'],
+        },
+        email: {
+            type: String,
+            required: [true, 'Email is required'],
+            validate: {
+                validator: val => /^([\w-\.]+@([\w-]+\.)+[\w-]+)?$/.test(val),
+                message: "Please enter a valid email"
+            }
+        },
+        password: {
+            type: String,
+            required: [true, 'Password is required'],
+            minlength: [8, "Password must be 8 characters or longer"]
+    },
+    },
+    {timestamps: true},
+);
